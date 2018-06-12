bb163484aabe8cb9d356d687b9c96b09d7030879
thishandp7 <thishandp7@gmail.com>
2018-06-11 23:53:56 -0500

    Improved test outputs and added test coverage

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -1,6 +1,22 @@
 from .base import *
 import os
 
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+    '--verbosity=2',
+    '--nologcapture',
+    '--with-coverage',
+    '--cover-package=todo',
+    '--with-spec',
+    '--spec-color',
+    '--with-xunit',
+    '--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+    '--cover-xml',
+    '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
 #Mysql database for test environment
 DATABASES = {
     'default': {
